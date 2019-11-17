7f7f8c5561e0a93ea2caef7822cf6bb3f98f0fd9
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-11-17 19:03:44 +0100

    get router almost done

--- src/router.rs
+++ src/router.rs
@@ -1,10 +1,11 @@
 extern crate futures;
 extern crate hyper;
 
-use futures::{future, Stream};
+use futures::future;
 use hyper::rt::Future;
 use hyper::{Body, Request, Response};
 use hyper::{Method, StatusCode};
+use std::fs;
 
 type FutureResponse = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
@@ -20,43 +21,36 @@ pub fn router(req: Request<Body>) -> FutureResponse {
 }
 
 fn get(response: &mut Response<Body>, path: &str) {
-    if path == "/" {
-        match std::fs::read_to_string("./public/index.html") {
-            Ok(file) => *response.body_mut() = Body::from(file),
+    match path {
+        "/all_the_route_with_no_hanlding_will_try_to_access_the_public_files" => {}
+        // might want to do a handle_static that sets the response itself and handles error
+        // in case image detection doesnt work as expected
+        _ => match get_static_asset(path) {
+            Ok(val) => *response.body_mut() = Body::from(val),
             Err(_e) => *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    } else if path == "/index.js" {
-        match std::fs::read_to_string("./public/index.js") {
-            Ok(file) => *response.body_mut() = Body::from(file),
-            Err(_e) => *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    } else {
-        *response.status_mut() = StatusCode::NOT_FOUND;
+        },
     }
 }
 
-// fn router_example(req: Request<Body>) -> FutureResponse {
-//     let mut response = Response::new(Body::empty());
-//
-//     match (req.method(), req.uri().path()) {
-//         (&Method::POST, "/echo/uppercase") => {
-//             let mapping = req.into_body().map(|chunk| {
-//                 chunk
-//                     .iter()
-//                     .map(|byte| byte.to_ascii_uppercase())
-//                     .collect::<Vec<u8>>()
-//             });
-//             *response.body_mut() = Body::wrap_stream(mapping);
-//         }
-//         (&Method::POST, "/echo/reverse") => {
-//             let reversed_mapping = req.into_body().concat2().map(move |chunk| {
-//                 let body = chunk.iter().rev().cloned().collect::<Vec<u8>>();
-//                 *response.body_mut() = Body::from(body);
-//                 response
-//             });
-//             return Box::new(reversed_mapping);
-//         }
-//     };
-//
-//     Box::new(future::ok(response))
-// }
+const FOLDER: &str = "./public";
+const DEFAULT_FILE: &str = "./public/index.html";
+
+fn get_static_asset(path: &str) -> Result<String, std::io::Error> {
+    for entry in fs::read_dir(FOLDER)? {
+        let direntry = entry?;
+        let mut filepath = format!("{:?}", direntry.path());
+        filepath.retain(|c| c != '"');
+
+        let filename = direntry.file_name().into_string().unwrap();
+        let mut extension = "/".to_owned();
+        extension.push_str(&filename);
+
+        // need to find how to detect it is an image and send back properly
+
+        if extension == path {
+            return Ok(std::fs::read_to_string(filepath)?);
+        }
+    }
+    Ok(std::fs::read_to_string(DEFAULT_FILE)?)
+}
+
