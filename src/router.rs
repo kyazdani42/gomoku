f79e556569d53a153fd99707cb2d8bf3147ae9a8
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-11-17 20:17:06 +0100

    refacto router

--- src/router.rs
+++ src/router.rs
@@ -6,6 +6,7 @@ use hyper::rt::Future;
 use hyper::{Body, Request, Response};
 use hyper::{Method, StatusCode};
 use std::fs;
+use std::io::Read;
 
 type FutureResponse = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
@@ -36,21 +37,21 @@ const FOLDER: &str = "./public";
 const DEFAULT_FILE: &str = "./public/index.html";
 
 fn get_static_asset(path: &str) -> Result<String, std::io::Error> {
-    for entry in fs::read_dir(FOLDER)? {
-        let direntry = entry?;
-        let mut filepath = format!("{:?}", direntry.path());
-        filepath.retain(|c| c != '"');
+    let mut entry = fs::read_dir(FOLDER)?
+        .map(|entry| entry.unwrap())
+        .find(|entry| entry.file_name().into_string().unwrap() == path[1..])
+        .map(|entry| format!("{:?}", entry.path()))
+        .unwrap_or(DEFAULT_FILE.to_owned());
+    entry.retain(|c| c != '"');
 
-        let filename = direntry.file_name().into_string().unwrap();
-        let mut extension = "/".to_owned();
-        extension.push_str(&filename);
-
-        // need to find how to detect it is an image and send back properly
-
-        if extension == path {
-            return Ok(std::fs::read_to_string(filepath)?);
-        }
+    // if its an image, do something else
+    if entry.ends_with(".jpg") || entry.ends_with(".jpeg") {
+        println!("its an image");
+        let file = fs::read(&entry)?
+            .iter()
+            .map(|chunk| chunk.to_string())
+            .collect::<Vec<String>>();
+        return Ok(file.join(""));
     }
-    Ok(std::fs::read_to_string(DEFAULT_FILE)?)
+    Ok(fs::read_to_string(entry)?)
 }
-
