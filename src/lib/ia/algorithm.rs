01b5969b2d2fad8b1c08f8e396641538eaf8e667
kiyan42 <yazdani.kiyan@protonmail.com>
2020-04-14 12:36:32 +0200

    simplify alphabeta

--- src/lib/ia/algorithm.rs
+++ src/lib/ia/algorithm.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 extern crate rand;
 use rand::Rng;
 
@@ -22,8 +23,8 @@ pub fn run(game: Game) -> Vec<Tile> {
     let mut best_hits = vec![];
     let empty_neighbours = game.empty_neighbours.clone();
 
-    let mut alpha = MIN;
-    let mut beta = MAX;
+    let alpha = RefCell::new(MIN);
+    let beta = RefCell::new(MAX);
     for tile in empty_neighbours {
         unsafe {
             analyzer_num += 1;
@@ -45,7 +46,7 @@ pub fn run(game: Game) -> Vec<Tile> {
         } else {
             let mut game = game.clone();
             game.update_game(&tile, &data.alignments, &data.captured);
-            best_hits.push((tile, alphabeta(&mut game, 9, &mut alpha, &mut beta, false)));
+            best_hits.push((tile, alphabeta(&mut game, 9, &alpha, &beta, false)));
         }
     }
 
@@ -57,118 +58,83 @@ pub fn run(game: Game) -> Vec<Tile> {
     }
 
     best_hits.sort_by(|a, b| a.1.cmp(&b.1));
-    for v in &best_hits {
-        println!("{} {}, h: {}", (v.0).0, (v.0).1, v.1);
-    }
+    // for v in &best_hits {
+    //     println!("{} {}, h: {}", (v.0).0, (v.0).1, v.1);
+    // }
     best_hits.iter().map(|v| v.0).collect()
 }
 
 fn alphabeta(
     game: &mut Game,
     depth: i32,
-    alpha: &mut i32,
-    beta: &mut i32,
+    alpha: &RefCell<i32>,
+    beta: &RefCell<i32>,
     maximizing_player: bool,
 ) -> i32 {
     if depth == 0 {
-        // return 1;
+        return 1;
         // return rand::thread_rng().gen();
-        return game.get_player().captured as i32
+        // return game.get_player().captured as i32;
     }
 
     let empty_neighbours = game.empty_neighbours.clone();
     let old_alignment = game.opponent_alignments.clone();
-    if maximizing_player {
-        let mut value = MIN;
-
-        for tile in &empty_neighbours {
-            unsafe {
-                analyzer_num += 1;
-            }
-            let now = Instant::now();
-            let data = game.analyze(tile);
-            unsafe {
-                analyzer_time += now.elapsed().as_nanos();
-            }
-
-            if data.double_free_three {
-                continue;
-            }
-
-            if data.win {
-                return MAX - depth * 1000;
-            } else if data.oponent_win {
-                return MIN + depth * 1000;
-            } else {
-                let now = Instant::now();
-                game.update_game(tile, &data.alignments, &data.captured);
-                unsafe {
-                    update_time += now.elapsed().as_nanos();
-                }
-
-                value = i32::max(
-                    alphabeta(game, depth - 1, alpha, beta, !maximizing_player),
-                    value,
-                );
-
-                let now = Instant::now();
-                game.reset_game(tile, &old_alignment, &data.captured, &empty_neighbours);
-                unsafe {
-                    reset_time += now.elapsed().as_nanos();
-                }
-
-                *alpha = i32::max(*alpha, value);
-                if *alpha >= *beta {
-                    break;
-                }
-            }
-        }
-        value
+    let (mut value, alpha_value) = if maximizing_player {
+        (MIN, alpha)
     } else {
-        let mut value = MAX;
+        (MAX, beta)
+    };
 
-        for tile in &empty_neighbours {
-            unsafe {
-                analyzer_num += 1;
-            }
-            let now = Instant::now();
-            let data = game.analyze(tile);
-            unsafe {
-                analyzer_time += now.elapsed().as_nanos();
-            }
+    let cmp_fn = if maximizing_player {
+        i32::max
+    } else {
+        i32::min
+    };
 
-            if data.double_free_three {
-                continue;
-            }
+    for tile in &empty_neighbours {
+        let now = Instant::now();
+        let data = game.analyze(tile);
+        unsafe {
+            analyzer_time += now.elapsed().as_nanos();
+            analyzer_num += 1;
+        }
 
-            if data.win {
-                return MIN + depth * 1000;
-            } else if data.oponent_win {
-                return MAX - depth * 1000;
+        if data.double_free_three {
+            continue;
+        } else if data.win {
+            return if maximizing_player {
+                MAX - depth * 1000
             } else {
-                let now = Instant::now();
-                game.update_game(tile, &data.alignments, &data.captured);
-                unsafe {
-                    update_time += now.elapsed().as_nanos();
-                }
+                MIN + depth * 1000
+            };
+        } else if data.oponent_win {
+            return if maximizing_player {
+                MIN + depth * 1000
+            } else {
+                MAX - depth * 1000
+            };
+        } else {
+            let now = Instant::now();
+            game.update_game(tile, &data.alignments, &data.captured);
+            unsafe {
+                update_time += now.elapsed().as_nanos();
+            }
+            value = cmp_fn(
+                alphabeta(game, depth - 1, alpha, beta, !maximizing_player),
+                value,
+            );
+            let now = Instant::now();
+            game.reset_game(tile, &old_alignment, &data.captured, &empty_neighbours);
+            unsafe {
+                reset_time += now.elapsed().as_nanos();
+            }
 
-                value = i32::min(
-                    alphabeta(game, depth - 1, alpha, beta, !maximizing_player),
-                    value,
-                );
-
-                let now = Instant::now();
-                game.reset_game(tile, &old_alignment, &data.captured, &empty_neighbours);
-                unsafe {
-                    reset_time += now.elapsed().as_nanos();
-                }
-
-                *beta = i32::min(*beta, value);
-                if *alpha >= *beta {
-                    break;
-                }
+            let v = *alpha_value.borrow();
+            alpha_value.replace(cmp_fn(v, value));
+            if *alpha >= *beta {
+                break;
             }
         }
-        value
     }
+    value
 }
